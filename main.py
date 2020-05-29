cc34dd89784220706d84489329a83082e490714b
GeneralUncertainty <data.kartikayerishi@gmail.com>
2020-05-29 10:12:55 +0530

    Python Password Generator

---
+++ main.py
@@ -0,0 +1,21 @@
+import string
+import random
+
+if __name__ == "__main__":
+    s1 = string.ascii_lowercase
+    s2 = string.ascii_uppercase
+    s3 = string.digits
+    s4 = string.punctuation
+
+    pass_len = int(input("Enter Password Length\n"))
+
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+    
+    random.shuffle(s)
+    
+    print('Your Password:\n')
+    print("".join(s[:pass_len]))
